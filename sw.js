d6cf04ec1e2ba024e0872f2f0a29b9b0287b6421
enAmne <afaurav@gmail.com>
2025-11-17 20:03:58 +0100

    Juego Yoselin Cluedo completo

---
+++ sw.js
@@ -0,0 +1,34 @@
+// Service Worker básico para cachear y permitir instalación/offline
+const NOMBRE_CACHE = 'yoselin-cluedo-v1';
+const RECURSOS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.webmanifest',
+  './assets/fondo.jpg',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (evento) => {
+  self.skipWaiting();
+  evento.waitUntil(
+    caches.open(NOMBRE_CACHE).then((cache) => cache.addAll(RECURSOS))
+  );
+});
+
+self.addEventListener('activate', (evento) => {
+  clients.claim();
+  evento.waitUntil(
+    caches.keys().then((claves) =>
+      Promise.all(claves.filter((c) => c !== NOMBRE_CACHE).map((c) => caches.delete(c)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((resp) => resp || fetch(evento.request))
+  );
+});
